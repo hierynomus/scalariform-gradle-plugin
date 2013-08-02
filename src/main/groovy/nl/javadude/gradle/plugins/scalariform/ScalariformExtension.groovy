9fe0ec74f42ddd0a117bc1a89006b131681b1d25
hierynomus <jeroen@javadude.nl>
2013-08-03 00:20:01 +0200

    Added some tests

--- src/main/groovy/nl/javadude/gradle/plugins/scalariform/ScalariformExtension.groovy
+++ src/main/groovy/nl/javadude/gradle/plugins/scalariform/ScalariformExtension.groovy
@@ -5,7 +5,7 @@ import scalariform.formatter.preferences.*
 class ScalariformExtension {
   def prefs = FormattingPreferences$.newInstance().apply()
 
-  def alignParameters(boolean v) {
+  def setAlignParameters(boolean v) {
     prefs = prefs.setPreference(AlignParameters$.newInstance(), v)
   }
 
@@ -13,75 +13,75 @@ class ScalariformExtension {
     prefs = prefs.setPreference(AlignSingleLineCaseStatements$.newInstance(), v)
   }
 
-  def alignSingleLineCaseStatements_maxArrowIndent(int i) {
+  def setAlignSingleLineCaseStatements_maxArrowIndent(int i) {
     prefs = prefs.setPreference(AlignSingleLineCaseStatements.MaxArrowIndent$.newInstance(), i)
   }
 
-  def compactControlReadability(boolean v) {
+  def setCompactControlReadability(boolean v) {
     prefs = prefs.setPreference(CompactControlReadability$.newInstance(), v)
   }
 
-  def compactStringConcatenation(boolean v) {
+  def setCompactStringConcatenation(boolean v) {
     prefs = prefs.setPreference(CompactStringConcatenation$.newInstance(), v)
   }
 
-  def doubleIndentClassDeclaration(boolean v) {
+  def setDoubleIndentClassDeclaration(boolean v) {
     prefs = prefs.setPreference(DoubleIndentClassDeclaration$.newInstance(), v)
   }
 
-  def formatXml(boolean v) {
+  def setFormatXml(boolean v) {
     prefs = prefs.setPreference(FormatXml$.newInstance(), v)
   }
 
-  def indentLocalDefs(boolean v) {
+  def setIndentLocalDefs(boolean v) {
     prefs = prefs.setPreference(IndentLocalDefs$.newInstance(), v)
   }
 
-  def indentPackageBlocks(boolean v) {
+  def setIndentPackageBlocks(boolean v) {
     prefs = prefs.setPreference(IndentPackageBlocks$.newInstance(), v)
   }
 
-  def indentSpaces(int i) {
+  def setIndentSpaces(int i) {
     prefs = prefs.setPreference(IndentSpaces$.newInstance(), i)
   }
 
-  def indentWithTabs(boolean v) {
+  def setIndentWithTabs(boolean v) {
     prefs = prefs.setPreference(IndentWithTabs$.newInstance(), v)
   }
 
-  def multilineScaladocCommentsStartOnFirstLine(boolean v) {
+  def setMultilineScaladocCommentsStartOnFirstLine(boolean v) {
     prefs = prefs.setPreference(MultilineScaladocCommentsStartOnFirstLine$.newInstance(), v)
   }
 
-  def preserveDanglingCloseParenthesis(boolean v) {
+  def setPreserveDanglingCloseParenthesis(boolean v) {
     prefs = prefs.setPreference(PreserveDanglingCloseParenthesis$.newInstance(), v)
   }
 
-  def placeScaladocAsterisksBeneathSecondAsterisk(boolean v) {
+  def setPlaceScaladocAsterisksBeneathSecondAsterisk(boolean v) {
     prefs = prefs.setPreference(PlaceScaladocAsterisksBeneathSecondAsterisk$.newInstance(), v)
   }
 
-  def preserveSpaceBeforeArguments(boolean v) {
+  def setPreserveSpaceBeforeArguments(boolean v) {
     prefs = prefs.setPreference(PreserveSpaceBeforeArguments$.newInstance(), v)
   }
 
-  def rewriteArrowSymbols(boolean v) {
+  def setRewriteArrowSymbols(boolean v) {
     prefs = prefs.setPreference(RewriteArrowSymbols$.newInstance(), v)
   }
 
-  def spaceBeforeColon(boolean v) {
+  def setSpaceBeforeColon(boolean v) {
     prefs = prefs.setPreference(SpaceBeforeColon$.newInstance(), v)
   }
 
-  def spaceInsideBrackets(boolean v) {
+  def setSpaceInsideBrackets(boolean v) {
     prefs = prefs.setPreference(SpaceInsideBrackets$.newInstance(), v)
   }
 
-  def spaceInsideParentheses(boolean v) {
+  def setSpaceInsideParentheses(boolean v) {
     prefs = prefs.setPreference(SpaceInsideParentheses$.newInstance(), v)
   }
 
-  def spacesWithinPatternBinders(boolean v) {
+  def setSpacesWithinPatternBinders(boolean v) {
     prefs = prefs.setPreference(SpacesWithinPatternBinders$.newInstance(), v)
   }
 }
